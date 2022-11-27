d3ce875fe358ae8bfeb03c6f128f6a4a011b0015
gonzalo-plaza <plazaruedag@gmail.com>
2022-11-27 15:45:24 +0100

    [ADD] Styles to appoinment list and logic to show the appoinments

--- js/appoinmentFunctions/functions.js
+++ js/appoinmentFunctions/functions.js
@@ -1,13 +1,32 @@
 'use strict'
-
+const appointmentBodySelector = document.querySelector('.js-appointments-body');
 export default function saveAppointment(Appointment){
     if(localStorage.appointments){
         let appointments = JSON.parse(localStorage.appointments);
         appointments[Appointment.id] = Appointment;
         localStorage.appointments = JSON.stringify(appointments);
+        updateAppoinmentsTable();
+       
     }else{
         let appointments = {};
         appointments[Appointment.id] = Appointment;
         localStorage.appointments = JSON.stringify(appointments);
+        updateAppoinmentsTable();
+    }
+}
+
+function updateAppoinmentsTable(){
+    debugger;
+    let appointments = JSON.parse(localStorage.appointments);
+    appointmentBodySelector.innerHTML= '';
+    for(const date in appointments){
+        let row = document.createElement('tr');
+        row.classList.add('appointments__row');
+        let appointmentDate = appointments[date].date.slice(0,-10);
+        let appointmentHour = appointments[date].date.slice(12,-3);
+        let columns = `<td>${appointmentDate}</td>
+        <td>${appointmentHour}</td><td>${appointments[date].name}</td><td>${appointments[date].surname}</td><td>${appointments[date].phone}</td>`;
+        row.innerHTML = columns;
+        appointmentBodySelector.appendChild(row);
     }
 }
--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import * as formValidation from './formValidation/commonFunctions.js';
 import  Appointment  from './classes/appointmentClass.js';
 import saveAppointment from './appoinmentFunctions/functions.js';
 
+document.addEventListener("DOMContentLoaded", updateAppoinmentsTable);
 const formNameField = document.querySelector('#name');
 const formSurnameField = document.querySelector('#surname');
 const formPhoneField = document.querySelector('#phone');
@@ -11,11 +12,16 @@ const formBirthField = document.querySelector('#birth');
 const formAppointmentDateField = document.querySelector('#appointmentDate');
 const formObservationsField = document.querySelector('#observations');
 const formSelector = document.querySelector('.js-form');
+const appointmentBodySelector = document.querySelector('.js-appointments-body');
+const todayDate = new Date();
+const newEvent = new Event('updateAppoint');
 
 formValidation.checkSimpleField(formNameField);
 formValidation.checkSimpleField(formSurnameField);
 formValidation.checkFieldNumber(formPhoneField);
 formValidation.checkFieldRegex(formIdNumberField, '^[0-9]{8}[a-zA-Z]$');
+formBirthField.setAttribute("max", todayDate.toLocaleDateString('en-CA'));
+formAppointmentDateField.setAttribute("min", todayDate.toISOString().slice(0,-8));
 
 formSelector.addEventListener('submit', function(e){
     e.preventDefault();
@@ -32,3 +38,18 @@ formSelector.addEventListener('submit', function(e){
     saveAppointment(appointment);
 });
 
+function updateAppoinmentsTable(){
+    debugger;
+    let appointments = JSON.parse(localStorage.appointments);
+    appointmentBodySelector.innerHTML= '';
+    for(const date in appointments){
+        let row = document.createElement('tr');
+        row.classList.add('appointments__row');
+        let appointmentDate = appointments[date].date.slice(0,-10);
+        let appointmentHour = appointments[date].date.slice(12,-3);
+        let columns = `<td>${appointmentDate}</td>
+        <td>${appointmentHour}</td><td>${appointments[date].name}</td><td>${appointments[date].surname}</td><td>${appointments[date].phone}</td>`;
+        row.innerHTML = columns;
+        appointmentBodySelector.appendChild(row);
+    }
+}
