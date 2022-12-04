7d8e0076d648e08bf8d3487caada9f5b116fcb6f
gonzalo-plaza <plazaruedag@gmail.com>
2022-12-04 13:18:07 +0100

    [ADD] Add format to do a short appointment ID

--- js/classes/appointmentClass.js
+++ js/classes/appointmentClass.js
@@ -4,7 +4,7 @@ export default class Appointment {
     constructor(name, surname, dni, phone, birth, date, observations){
         let appointmentDate = new Date(date);
 
-        this.id = String(new Date());
+        this.id = String(new Date().toISOString());
         this.name = name;
         this.surname = surname;
         this.dni = dni;
